8e42fb6129b5db425ecc9b98e61bbb98d976b268
WildisNabut <wildisnabut24@gmail.com>
2024-11-12 10:41:22 +0800

    verivikasi

--- Admin/Hapus_renungan.php
+++ Admin/Hapus_renungan.php
@@ -2,6 +2,6 @@
 include "../koneksi.php";
 
 $Kode = $_GET['kode'];
-mysqli_query($koneksi, "delete from guru where id_renungan='$Kode'");
+mysqli_query($koneksi, "delete from guru where judul='$Kode'");
 header('location:renungan.php');
 ?>
--- napigasi.php
+++ napigasi.php
@@ -24,7 +24,6 @@
 		if (empty($_SESSION['username'])) {
 			echo "
 			<ul class='agile_forms'>
-				<li><a class='active' href='#' data-toggle='modal' data-target='#myModal2'><i class='fa fa-sign-in' aria-hidden='true'></i> Masuk </a></li>
 			</ul>";
 		} else {
 			echo "
@@ -108,38 +107,5 @@
 		</nav>	
 		<div class="clearfix"> </div> 
 	</div>
-</div>
-
-<!-- Modal1 -->
-<div class="modal fade" id="myModal2" tabindex="-1" role="dialog">
-	<div class="modal-dialog">
-	<!-- Modal content-->
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal">&times;</button>
-				<div class="signin-form profile">
-					<h3 class="agileinfo_sign">Masuk</h3>	
-					<div class="login-form">
-						<form action="proses_login.php" method="post">
-							<input type="text" name="username" placeholder="Username" required="">
-							<input type="password" name="password" placeholder="Password" required="">
-							<div class="tp">
-								<input type="submit" value="Masuk">
-							</div>
-						</form>
-					</div>
-					<div class="login-social-grids">
-						<ul>
-							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-							<li><a href="#"><i class="fa fa-rss"></i></a></li>
-						</ul>
-					</div>
-					<p><a href="#" data-toggle="modal" data-target="#myModal3" > Don't have an account?</a></p>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-<!-- //Modal1 -->	
+</div>	
 
